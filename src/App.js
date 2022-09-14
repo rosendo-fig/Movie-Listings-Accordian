da2141998fc4f6765dd5d6b3db66280c096b5cb9
Rosendo Figueroa <rosendo.figueroa@chandleraz.gov>
2022-09-14 11:02:40 -0700

    Updated Styles Header, Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Accordion from "react-bootstrap/Accordion";
+import data from "./data.json";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <Container>
+        <Row>
+          <Col sm={12}>
+            <Accordion>
+              {data.map((movie) => {
+                return (
+                  <Accordion.Item eventKey={movie.id}>
+                    <Accordion.Header>{movie.name}</Accordion.Header>
+                    <Accordion.Body>
+                      <Row>
+                        <Col sm={8}>
+                          <h3>{movie.name}</h3>
+                          <p>{movie.synopsis}</p>
+                        </Col>
+                        <Col sm={4}>
+                          <h6>{movie.cast}</h6>
+                          <p>Genre: {movie.genre}</p>
+                          <p>Rating: {movie.rating}</p>
+                          <p>Release {movie.releaseDate}</p>
+                        </Col>
+                      </Row>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                );
+              })}
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <Container fluid className="footer">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </Container>
+  );
+};
+
+export default Footer;
